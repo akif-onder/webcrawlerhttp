ceddfed20bb32f5d48070b16f3e72eb1774e2319
akif onder <akifonder@akifs-MacBook-Pro.local>
2023-02-25 15:46:26 +0300

    getURLsFromHTML function and tests

--- crawl.js
+++ crawl.js
@@ -1,4 +1,34 @@
-function normalizeURL(urlString) {
+const {JSDOM} = require('jsdom')
+
+function getURLsFromHTML(htmlBody, baseURL) {
+    const urls = []
+    const dom = new JSDOM(htmlBody)
+    const linkElements = dom.window.document.querySelectorAll('a')
+    for (const linkElement of linkElements) {
+        if (linkElement.href.slice(0,1) === '/') {
+            // relative url
+            try {
+                const urlObj = new URL(`${baseURL}${linkElement.href}`)
+                urls.push(urlObj.href)
+            } catch (error) {
+                console.log(`error with relative url: ${error.message}`);
+            }
+            
+        } else {
+            // absolute url
+            try {
+                const urlObj = new URL(linkElement.href)
+                urls.push(urlObj.href)
+            } catch (error) {
+                console.log(`error with relative url: ${error.message}`);
+            }
+        }   
+    }
+    return urls
+}
+
+
+function normalizeURL(urlString) { 
     const urlObj = new URL(urlString)
     const hostPath =  `${urlObj.hostname}${urlObj.pathname}`
     if (hostPath.length > 1 && hostPath.slice(-1) === '/') {
@@ -8,5 +38,6 @@ function normalizeURL(urlString) {
  }
 
  module.exports = {
-    normalizeURL
+    normalizeURL,
+    getURLsFromHTML
  }
